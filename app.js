2ade2ec32e93b2064c70f4e6cedc046706e23115
Gabe Conradi <gabe@tumblr.com>
2014-05-08 07:51:47 -0400

    primus websockets working with ping+pong

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var routes = require('./routes');
 var http = require('http');
 var path = require('path');
 var assets_api = require('./routes/api/assets');
+var Primus = require('primus');
 
 var app = express();
 
@@ -31,6 +32,35 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/api/assets', assets_api.assets);
 
-http.createServer(app).listen(app.get('port'), function(){
+// web server from the express instance
+var server = http.createServer(app);
+// create primus instance from webserver
+var primus = Primus(server,{transformer: 'engine.io'})
+.on('connection',function(spark){
+  console.log("connection");
+  spark.write("hello");
+  spark.on('data',function(d){
+    console.log("got client data: " + d);
+    if (d === "pong"){
+      console.log("pong, pinging in 1s");
+      setTimeout(function(){
+        spark.write("ping");
+      },1000);
+    } else if (d === "hi") {
+      console.log("client said hi, starting ping");
+      setTimeout(function(){
+        spark.write("ping");
+      },1000);
+    }
+  });
+})
+.on('disconnect',function(spark){
+  console.log("got disconnect");
+});
+
+server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+
+
