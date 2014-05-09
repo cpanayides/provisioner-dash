be17e895d17727598a362ff7199d2f23809e47de
Gabe Conradi <gabe@tumblr.com>
2014-05-08 21:39:36 -0400

    add testing script

--- scripts/test_provision.sh
+++ scripts/test_provision.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
-tag=tumblrtag305
+tag="${1:-tumblrtag305}"
+firehose="http://localhost:3000/firehose/asset/${tag}"
+collins="http://localhost:9000/api/asset/${tag}/status"
+
 echo "Moving ${tag} to unallocated"
-curl --basic -u blake:admin:first -d status=Unallocated -d state=Running -d reason='gabe unallocating' "http://localhost:9000/api/asset/${tag}/status"
+curl --basic -u blake:admin:first -d status=Unallocated -d state=Running -d reason='gabe unallocating' $collins
+curl --data-urlencode "" $firehose
 echo "sleeping 10s"
 sleep 10s
 
 echo "Moving ${tag} to provisioning"
-curl --basic -u blake:admin:first -d status=Provisioning -d state=Running -d reason='gabe provisioning' "http://localhost:9000/api/asset/${tag}/status"
+curl --basic -u blake:admin:first -d status=Provisioning -d state=Running -d reason='gabe provisioning' $collins
+curl --data-urlencode "" $firehose
 echo "sleeping 10s"
 sleep 10s
 
 echo "Moving ${tag} to provisioned"
-curl --basic -u blake:admin:first -d status=Provisioned -d state=Running -d reason='gabe provisioning' "http://localhost:9000/api/asset/${tag}/status"
+curl --basic -u blake:admin:first -d status=Provisioned -d state=Running -d reason='gabe provisioning' $collins
+curl --data-urlencode "" $firehose
 echo "sleeping 10s"
 sleep 10s
 
 echo "Moving ${tag} to Allocated"
-curl --basic -u blake:admin:first -d status=Allocated -d state=Running -d reason='gabe allocating' "http://localhost:9000/api/asset/${tag}/status"
-echo "sleeping 10s"
-sleep 10s
+curl --basic -u blake:admin:first -d status=Allocated -d state=Running -d reason='gabe allocating' $collins
+curl --data-urlencode "" $firehose
